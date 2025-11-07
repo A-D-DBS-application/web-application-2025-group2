aea62aa40f62bcd41a734069b7f248654d5700a6
sangii99 <Sang.Yoon@ugent.be>
2025-11-07 14:55:13 +0100

    login page trial 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,27 @@
-import os
+from flask import Flask
+from .models import db
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()  # Create sql tables for our data models
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
+
+
+
+
+
+
+'''import os
 from pathlib import Path
 
 from flask import Flask
@@ -62,7 +85,7 @@ def create_app():
 
     print("✅ Flask app configured and DB tables ensured.")
     return app
-
+'''
 
 
 
