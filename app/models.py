81bd050d28e8987164e51191c344eb79c321d112
sangii99 <Sang.Yoon@ugent.be>
2025-11-20 16:59:10 +0100

    error3

--- app/models.py
+++ app/models.py
@@ -127,3 +127,9 @@ class KVStore(db.Model):
         return f'<KVStore {self.key}>'
 
 
+@app.route('/login')
+def login():
+    # This is a placeholder for the login function
+    return 'Login page'
+
+
