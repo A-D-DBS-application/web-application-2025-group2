335b48cddc63ebd4fcdc275c8133849b4560203a
sangii99 <Sang.Yoon@ugent.be>
2025-11-29 19:56:43 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,17 +2,5 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-<<<<<<< HEAD
+if __name__ == '__main__':
     app.run(debug=True)
-
-
-<<<<<<< HEAD
-print("hello world")
-=======
-# change te
->>>>>>> b1e16abe47b8fd5aea4ab26ba5375b418c777c96
-
-=======
-    app.run(debug=True, port=3000)
->>>>>>> 04ae79da4cf010bb2ecd33a107dcdb337d58893f
