90abf75a8c371bd3303510efc3355d0f85e42bef
stefvandevelde05-commits <stefvandevelde05@gmail.com>
2025-12-08 13:31:30 +0100

    error fix

--- run.py
+++ run.py
@@ -3,12 +3,7 @@ import os
 
 app = create_app()
 
-<<<<<<< HEAD
 if __name__ == "__main__":
     # Use PORT env var if present (useful for hosting), default to 5000
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
-=======
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 6cdc61bcf744dc751babcbf5016d05c0c31e8632
