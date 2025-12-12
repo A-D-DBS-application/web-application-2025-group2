d366c866fd65cd9f0b1e6c38a5c6058bb3d34140
LeonHuysman <Leon.huysman8@gmail.com>
2025-12-12 21:13:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DarkModeToggle from "./components/DarkModeToggle";
+import YourComponent from "./components/YourComponent";
 
 function App() {
   return (
     <>
       <DarkModeToggle />
+      <YourComponent />
       {/* ...existing code... */}
     </>
   );
